a48dc46ed0a5fd41b73c28e7c985865b3ae2d60f
Bradley Blue <b_blue@mail.fhsu.edu>
2023-12-07 22:05:33 -0600

    Final commit for submission

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Question #1
 const createElemWithText = (html = 'p',txtContent = '', className) => {
     const createdElem = document.createElement(html);
-    createdElem.innerText = txtContent;
+    createdElem.textContent = txtContent;
     if (className !== undefined) {
         createdElem.classList.add(className);
     }
@@ -61,13 +61,13 @@ const deleteChildElements = (parentElem) => {
 
 // Question #6
 const addButtonListeners = () => {
-    const btns = document.querySelectorAll('button');
-    if (btns !== null) {
+    const btns = document.querySelectorAll('button:not(.testLinkButton)');
+    if (btns !== undefined) {
         btns.forEach((btn) => {
             let postId = btn.dataset.postId;
             if (postId !== null) {
                 btn.addEventListener('click', (event) => {
-                    toggleCommentS(event, postId);
+                    toggleComments(event, postId);
                 }, false)
             }
         })
@@ -75,14 +75,12 @@ const addButtonListeners = () => {
     return btns;
 }
 
-addButtonListeners();
-
 // Question #7
 const removeButtonListeners = () => {
-    const btns = document.querySelectorAll('button');
+    const btns = document.querySelectorAll('button:not(.testLinkButton)');
     if (btns !== null) {
         btns.forEach((btn) => {
-            let postId = btn.dataset.postId;
+            let postId = btn.dataset.id;
             if (postId !== null) {
                 btn.removeEventListener('click', (event) => {
                 }, false)
@@ -100,30 +98,180 @@ const createComments = (jsonComm) => {
     jsonComm.forEach((comm) => {
         let article = document.createElement('article');
         let h3 = createElemWithText('h3', comm.name);
-        console.log(h3);
         let body = createElemWithText('p', comm.body);
-        console.log(body);
         let from = createElemWithText('p', `From: ${comm.email}`);
-        console.log(from);
         article.append(h3, body, from);
         fragment.append(article);
     })
     return fragment;
 }
 
-//createComments(jsonText);
+// Question #9
+const populateSelectMenu = (jsonData) => {
+    if (!jsonData) return;
+    const SelectMenu = document.getElementById('selectMenu');
+    const optElements = createSelectOptions(jsonData);
+    optElements.forEach((element) => {
+        SelectMenu.append(element);
+    })
+    return SelectMenu;
+}
+
+
+// Question #10
+const getUsers = async () => {
+    let jsonData;
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        jsonData =  await response.json();
+    } catch(error) {
+        console.log(error);
+    }
+    return jsonData;
+}
+
+// Question #11
+const getUserPosts = async (userId) => {
+    if (!userId) return;
+    let jsonData;
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
+        jsonData =  await response.json();
+    } catch(error) {
+        console.log(error);
+    }
+    return jsonData;
+}
+
+// Question #12
+const getUser = async (userId) => {
+    if (!userId) return;
+    let jsonData;
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        jsonData =  await response.json();
+    } catch(error) {
+        console.log(error);
+    }
+    return jsonData;
+}
+
 
 // Question #13
 const getPostComments = async (postId) => {
     if (!postId) return;
+    let jsonData;
     try {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
+        jsonData = await response.json();
     } catch(error) {
         console.log(error);
     }
-    return response;
+    return jsonData;
 }
 
-const toggleCommentS = (event, postId) => {
-
+// Question #14
+const displayComments = async (postId) => {
+    if (!postId) return;
+    const section = document.createElement('section');
+    section.dataset.postId = postId;
+    section.classList.add('comments', 'hide');
+    const comments = await getPostComments(postId);
+    const fragment = createComments(comments);
+    section.append(fragment);
+    return section;
 }
+
+// Question #15
+const createPosts = async (jsonData) => {
+    if (!jsonData) return;
+    const fragment = document.createDocumentFragment();
+    let article;
+    for (let i = 0; i < jsonData.length; i++) {
+        let data = jsonData[i];
+        article = document.createElement('article');
+        let h2 = createElemWithText('h2', data.title);
+        let para = createElemWithText('p', data.body);
+        let paraPostId = createElemWithText('p', `Post ID: ${data.id}`);
+        let author = await getUser(data.userId);
+        let paraAuthor = createElemWithText('p', `Author: ${author.name} with ${author.company.name}`);
+        let paraAuthCatchPhrase = createElemWithText('p', author.company.catchPhrase);
+        let btn = createElemWithText('button', 'Show Comments');
+        btn.dataset.postId = data.id;
+        article.append(h2, para, paraPostId, paraAuthor, paraAuthCatchPhrase, btn);
+        let section = await displayComments(data.id);
+        article.append(section);
+        fragment.append(article);
+    }
+    return fragment;
+}
+
+// Question #16
+const displayPosts = async (postData) => {
+    const main = document.getElementsByTagName('main')[0];
+    let element;
+    if (postData) {
+        element = await createPosts(postData)
+    } else {
+        element = createElemWithText('p', document.getElementsByClassName('default-text')[0].textContent, 'default-text')
+    }
+    main.append(element);
+    return element;
+}
+
+// Question #17
+const toggleComments = (event, postId) => {
+    if (!event || !postId) return;
+    event.target.listener = true;
+    const sect = toggleCommentSection(postId);
+    const btn = toggleCommentButton(postId);
+    const array = [sect, btn];
+    return array;
+}
+
+// Question #18
+const refreshPosts = async (jsonPostData) => {
+    if (!jsonPostData) return;
+    const btnRemoveArray = removeButtonListeners();
+    const mainElem = deleteChildElements(document.getElementsByTagName('main')[0]);
+    const fragment = await displayPosts(jsonPostData);
+    const btnAddArray = addButtonListeners();
+    const array = [btnRemoveArray, mainElem, fragment, btnAddArray];
+    return array;
+}
+
+// Question #19
+const selectMenuChangeEventHandler = async (event) => {
+    if (!event) return;
+    document.getElementById('selectMenu').disabled = true;
+    const userId = event?.target?.value || 1;
+    const jsonPostData = await getUserPosts(userId);
+    const refreshPostsArray = await refreshPosts(jsonPostData);
+    document.getElementById('selectMenu').disabled = false;
+    const array = [userId, jsonPostData, refreshPostsArray];
+    return array;
+}
+
+// Question #20
+const initPage = async () => {
+    const jsonUserData = await getUsers();
+    const selectElem = populateSelectMenu(jsonUserData);
+    const array = [jsonUserData, selectElem];
+    return array;
+}
+
+
+// Question #21
+const initApp = () => {
+    initPage();
+    const selectMenu = document.getElementById('selectMenu');
+    selectMenu.addEventListener('change', (event) => {
+        selectMenuChangeEventHandler(event);
+    }, false);
+}
+
+document.addEventListener('readystatechange', (event) => {
+    if (event.target.readyState === 'complete') {
+        initApp();
+    }
+})
